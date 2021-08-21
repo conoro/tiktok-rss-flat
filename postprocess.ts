ab18146a0e82e816573a5f11b748c4b2cd439d0b
Conor ONeill <conor@conoroneill.com>
2021-08-21 16:22:37 +0100

    attempt to deal with two errors in the logs

--- postprocess.ts
+++ postprocess.ts
@@ -1,3 +1,9 @@
+const apt_update = Deno.run({
+  cmd: ['sudo', 'apt-get', 'update'],
+});
+
+await apt_update.status();
+
 const apt_install = Deno.run({
   cmd: ['sudo', 'apt-get', 'install', "libegl1", "libopus0", "libwoff1", "libharfbuzz-icu0", "gstreamer1.0-plugins-base", "libgstreamer-gl1.0-0", "gstreamer1.0-plugins-bad", "libopenjp2-7", "libwebpdemux2", "libenchant1c2a", "libhyphen0", "libgles2", "gstreamer1.0-libav", "libevdev-dev"],
 });
@@ -17,6 +23,13 @@ const playwright_install = Deno.run({
 });
 
 await playwright_install.status();
+
+const playwright_configure = Deno.run({
+  cmd: ['sudo', 'npx', 'playwright', 'install-deps'],
+});
+
+await playwright_configure.status();
+
 
 // Forwards the execution to the python script
 const py_run = Deno.run({
