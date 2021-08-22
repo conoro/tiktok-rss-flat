bfb08f49c90a992c114e434e5495e1ad37b33b23
Conor ONeill <conor@conoroneill.com>
2021-08-22 16:58:21 +0100

    third attempt to deal with two errors in the logs

--- postprocess.ts
+++ postprocess.ts
@@ -1,9 +1,3 @@
-//const apt_update = Deno.run({
-//  cmd: ['sudo', 'apt-get', 'update'],
-//});
-
-//await apt_update.status();
-
 const apt_install = Deno.run({
   cmd: ['sudo', 'apt-get', 'install', "libegl1", "libopus0", "libwoff1", "libharfbuzz-icu0", "gstreamer1.0-plugins-base", "libgstreamer-gl1.0-0", "gstreamer1.0-plugins-bad", "libopenjp2-7", "libwebpdemux2", "libenchant1c2a", "libhyphen0", "libgles2", "gstreamer1.0-libav", "libevdev-dev"],
 });
