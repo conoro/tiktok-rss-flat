f1cedd245691c8ce06400a7d7212158def901700
Gabriel Neuman <soy@gabrielneuman.com>
2022-01-03 20:11:14 -0600

    Update tiktok_example_json.py

--- tiktok_example_json.py
+++ tiktok_example_json.py
@@ -5,7 +5,7 @@ api = TikTokApi.get_instance()
 
 count = 1
 
-tiktoks = api.by_username("iamtabithabrown", count=count)
+tiktoks = api.by_username("gneumanmx", count=count)
 
 jsonString = json.dumps(tiktoks)
 jsonFile = open("tiktok_example_data.json", "w")
