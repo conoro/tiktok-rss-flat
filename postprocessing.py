33e61bfe8e524ed38d008434963fd2c67fd10b54
Gabriel Neuman <soy@gabrielneuman.com>
2022-01-03 18:56:02 -0600

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://gneuman.github.io/tiktok-rss-flat/"
 
 api = TikTokApi.get_instance()
 
