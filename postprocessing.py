ea372035c9ddc9467d3786c3ce9cfb8723e55b14
Conor ONeill <conor@conoroneill.com>
2024-08-08 13:51:04 +0100

    added screenshotting to create proper thumbnails

--- postprocessing.py
+++ postprocessing.py
@@ -1,11 +1,16 @@
 import os
 import asyncio
 import csv
+import time
 from datetime import datetime, timezone
 from feedgen.feed import FeedGenerator
 #from tiktokapipy.api import TikTokAPI
 from TikTokApi import TikTokApi
 import config
+from playwright.async_api import async_playwright, Playwright
+from pathlib import Path
+from urllib.parse import urlparse
+
 
 # Normal GitHub Pages URL
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
@@ -18,6 +23,15 @@ api = TikTokApi()
 ms_token = os.environ.get(
     "MS_TOKEN", None
 )
+
+async def runscreenshot(playwright: Playwright, url, screenshotpath):
+    chromium = playwright.chromium # or "firefox" or "webkit".
+    browser = await chromium.launch()
+    page = await browser.new_page()
+    await page.goto(url)
+    # Save the screenshot
+    await page.screenshot(path=screenshotpath, quality = 20, type = 'jpeg')
+    await browser.close()
 
 async def user_videos():
 
@@ -44,33 +58,48 @@ async def user_videos():
             async with TikTokApi() as api:
                 await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False)
                 ttuser = api.user(user)
-                user_data = await ttuser.info()
-                #print(user_data)
+                try:
+                    user_data = await ttuser.info()
+                    #print(user_data)
 
-                async for video in ttuser.videos(count=10):
-                    fe = fg.add_entry()
-                    link = "https://tiktok.com/@" + user + "/video/" + video.id
-                    fe.id(link)
-                    ts = datetime.fromtimestamp(video.as_dict['createTime'], timezone.utc)
-                    fe.published(ts)
-                    fe.updated(ts)
-                    updated = max(ts, updated) if updated else ts
-                    if video.as_dict['desc']:
-                        fe.title(video.as_dict['desc'][0:255])
-                    else:
-                        fe.title("No Title")
-                    fe.link(href=link)
-                    if video.as_dict['desc']:
-                        description = video.as_dict['desc'][0:255]
-                    else:
-                        description="No Description"
-                    if video.as_dict['video']['cover']:
-                        description = '<img src="' + video.as_dict['video']['cover'] + '" / >' + description    
-                    fe.description(description)        
-                fg.updated(updated)
-                fg.atom_file('rss/' + user + '.xml', pretty=True) # Write the RSS feed to a file
-                    #print(video)
-                    #print(video.as_dict)
+                    async for video in ttuser.videos(count=10):
+                        fe = fg.add_entry()
+                        link = "https://tiktok.com/@" + user + "/video/" + video.id
+                        fe.id(link)
+                        ts = datetime.fromtimestamp(video.as_dict['createTime'], timezone.utc)
+                        fe.published(ts)
+                        fe.updated(ts)
+                        updated = max(ts, updated) if updated else ts
+                        if video.as_dict['desc']:
+                            fe.title(video.as_dict['desc'][0:255])
+                        else:
+                            fe.title("No Title")
+                        fe.link(href=link)
+                        if video.as_dict['desc']:
+                            description = video.as_dict['desc'][0:255]
+                        else:
+                            description="No Description"
+                        if video.as_dict['video']['cover']:
+                            videourl = video.as_dict['video']['cover']
+                            parsed_url = urlparse(videourl)
+                            path_segments = parsed_url.path.split('/')
+                            last_segment = [seg for seg in path_segments if seg][-1]
+
+                            screenshotsubpath = "/thumbnails/" + user + "/screenshot_" + last_segment + ".jpg"
+                            screenshotpath = os.path.dirname(os.path.realpath(__file__))+screenshotsubpath
+                            if not os.path.isfile(screenshotpath):
+                                async with async_playwright() as playwright:
+                                    await runscreenshot(playwright, videourl, screenshotpath)
+                                # RAW URL of that is something like: https://conoro.github.io/tiktok-rss-flat/thumbnails/iamtabithabrown/screenshot_786542134.jpg
+                            screenshoturl =  "https://conoro.github.io/tiktok-rss-flat" + screenshotsubpath
+                            description = '<img src="' + screenshoturl + '" / >' + description    
+                        fe.description(description)
+                    fg.updated(updated)
+                    fg.atom_file('rss/' + user + '.xml', pretty=True) # Write the RSS feed to a file
+                        #print(video)
+                        #print(video.as_dict)
+                except Exception as e:
+                    print(e)
 
 
 if __name__ == "__main__":
