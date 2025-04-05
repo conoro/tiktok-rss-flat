0ab2de003429b1c6987630d3133cc904f510943c
Deb9503 <segodeborah2001@gmail.com>
2025-04-05 16:42:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+ghPagesURL = "https://deb9503.github.io/tiktok-rss-flat/"
+ghRawURL = "https://raw.githubusercontent.com/deb9503/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
